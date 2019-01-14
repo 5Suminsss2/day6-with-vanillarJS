882866c257efd1aa2e3dc92bbf8faf2f4f2e8905
5Suminsss2 <1012ksm@naver.com>
2019-01-14 13:45:15 +0900

    button수정하기
    오류난거 수정했음

--- timer.js
+++ timer.js
@@ -1,8 +1,23 @@
 const clockTimer = document.querySelector("#timer");
 const buttonChange = document.querySelector("#buttonChange1");
 const buttonChange2 = document.querySelector("#buttonChange2");
+var parent = document.getElementById('buttonChange1');
+var customElement = createCustomElement("Click M/V", "https://www.youtube.com/embed/1nk5O__ALI8");
+
+
+function createCustomElement(anchorText, anchorLink){
+
+  var aTag = document.createElement("a");
+  aTag.href = anchorLink;
+  aTag.innerHTML = anchorText;
+  aTag.classList.add("wrap");
+  aTag.classList.add("button");
+  return aTag ;
+}
+
+
 function getTime() {
-  const countDownDate = new Date("Jan 13, 2019 15:13:50");
+  const countDownDate = new Date("Jan 30, 2018 13:15:00");
   const now = new Date();
 
   var distance = countDownDate - now;
@@ -10,26 +25,25 @@ function getTime() {
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000 +1);
 
   clockTimer.innerText = days + "D " + hours + "H "
   + minutes + "M " + seconds + "S ";
-
+ 
   if(distance < 0){
     clearInterval(getTime);
     clockTimer.innerText = "TIME OUT";
-    buttonChange2.innerText = "Click M/V";
-    buttonChange.classList.add("wrap");
-    buttonChange2.classList.add("button");
-
-  
+    parent.appendChild(customElement);
   }
 }
+
 
 function init() {
   getTime();
   setInterval(getTime,1000);
 
 }
+
+
 
 init();
